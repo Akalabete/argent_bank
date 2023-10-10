4011ae10f4eab2dda675d47d203b236f7d7b9de5
Alexandre LACOUR <100684188+Akalabete@users.noreply.github.com>
2023-10-10 14:50:56 +0200

    user registration working now

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,41 +1,70 @@
 'use client';
+import { useRouter } from 'next/navigation';
 import {  updateRegistrationFormField } from '@/redux/features/formSlice'
 import { useAppDispatch, useAppSelector } from '@/redux/hook';  
 import styles from './page.module.scss';
+import { openModal, closeModal } from '../../redux/features/modalSlice';
+import Modal from '../../component/modal/page'
+import { setConnected } from "../../redux/features/userSlice";
+
+
+
 
 export default function RegistrationForm() {
   const registrationForm = useAppSelector((state) => state.form.registrationData);
   const dispatch = useAppDispatch();
+  const modal = useAppSelector((state: { modal: any; }) => state.modal);
+  const router = useRouter()
+
+    const handleOpenModal = () => {
+      dispatch(openModal({ title: 'Modal Title', message: 'Modal Message' }));
+    };
+
+    const handleCloseModal = () => {
+      dispatch(closeModal());
+      router.push(`/`)
+    };
+    
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     dispatch(updateRegistrationFormField({ fieldName: name, fieldValue: value }));
   };
   const handleRegistrationSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    const registrationFormData = {
-      "email" : registrationForm.email,
-      "password" : registrationForm.password,
-      "firstName": registrationForm.firstname,
-      "lastName": registrationForm.lastname,
-      "userName": registrationForm.username,
-    };
-    console.log(registrationFormData)
     try {
-      const response = await fetch("http://localhost:3001/api/v1/user/signup", {
+      const newUserResponse = await fetch("http://localhost:3001/api/v1/user/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
           "accept":"application/json"
         },
-        body: JSON.stringify({registrationFormData}),
+        body: JSON.stringify({
+          "email" : registrationForm.email,
+          "password" : registrationForm.password,
+          "firstName": registrationForm.firstname,
+          "lastName": registrationForm.lastname,
+          "userName": registrationForm.username,
+        }),
       });
-      if(response.status === 200) {
-        dispatch(updateRegistrationFormField({ fieldName: "firstName", fieldValue: "" }));
-        dispatch(updateRegistrationFormField({ fieldName: "lastName", fieldValue: "" }));
+      if(newUserResponse.status === 200) {
+        dispatch(updateRegistrationFormField({ fieldName: "firstname", fieldValue: "" }));
+        dispatch(updateRegistrationFormField({ fieldName: "lastname", fieldValue: "" }));
         dispatch(updateRegistrationFormField({ fieldName: "email", fieldValue: "" }));
         dispatch(updateRegistrationFormField({ fieldName: "password", fieldValue: "" }));
-        dispatch(updateRegistrationFormField({ fieldName: "userName", fieldValue: "" }));
+        dispatch(updateRegistrationFormField({ fieldName: "username", fieldValue: "" }));
         console.log("cheer");
+        dispatch(setConnected(true));
+        //const profileData = await newUserResponse.json();
+        //const customId = profileData.body.id;
+        sessionStorage.setItem("profile", JSON.stringify(profileData));
+        dispatch(
+          openModal({
+            title: "Success",
+            message: "User successfully registered",
+
+          })
+        );
+        
       }
 
     } catch(error){
@@ -44,6 +73,15 @@ export default function RegistrationForm() {
   }
   return(
     <>
+    
+    {modal.isOpen && (
+        <Modal
+          isOpen={modal.isOpen}
+          title={modal.title}
+          message={modal.message}
+          onClose={handleCloseModal}
+        />
+      )}   
     <h2>Please fill the registration form below:</h2>
     <div className={styles.loginWindow}>
       
